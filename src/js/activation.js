69af4764436302b711fed1dc2ec3ac3fb2c24643
REDMOND\shweaver <shweaver@microsoft.com>
2018-03-22 15:36:34 -0700

    initial commit

---
+++ src/js/activation.js
@@ -0,0 +1,69 @@
+﻿(function () {
+
+    var ActivationService = function () {
+
+        // private
+        function onLaunched(args) {
+            console.log("Launch activated");
+
+            var activation = Windows.ApplicationModel.Activation;
+
+            if (args && (args.previousExecutionState == activation.ApplicationExecutionState.terminated || args.previousExecutionState == activation.ApplicationExecutionState.closedByUser)) {
+                // TODO: Populate the UI with the previously saved application data
+            }
+            else {
+                // TODO: Populate the UI with defaults
+            }
+        }
+
+        // private
+        function onShareTargetActivated(args) {
+            console.log("ShareTarget Activated");
+        }
+
+        // private
+        function onContactPanelActivated(args) {
+            console.log("ContectPanel Activated");
+        }
+
+        // private
+        function onWebActivated(args) {
+            console.log("Web Activated");
+        }
+
+        // The most recent activation args
+        this.LastActivationArgs;
+
+        // Handles app activation
+        this.OnActivated = function (args) {
+            console.log("OnActivated");
+
+            this.LastActivationArgs = args;
+
+            if (!window.Windows) {
+                onWebActivated(args);
+                return;
+            }
+
+            if (args && args.kind != undefined) {
+
+                var activation = Windows.ApplicationModel.Activation;
+
+                switch (args.kind) {
+                    case activation.ActivationKind.launch: onLaunched(args); return;
+                    case activation.ActivationKind.shareTarget: onShareTargetActivated(args); return;
+                    case activation.ActivationKind.contactPanel: onContactPanelActivated(args); return;
+                }
+
+                console.log("Unhandled activation kind: " + args.kind);
+            }
+            else {
+                console.log("Unhandled activation: Missing args - " + args);
+            }
+        }.bind(this);
+    };
+
+    var activationService = new ActivationService();
+    document.activation = activationService;
+
+})();
---
+++ src/js/mypeople.js
@@ -0,0 +1,51 @@
+﻿(function () {
+
+    var MyPeopleService = function () {
+
+        this.CreateContact = function (contactId, firstName, lastName, email, phone) {
+            if (!window.Windows) {
+                console.log("MyPeople is not supported on web");
+                return;
+            }
+
+            var apiInformation = Windows.Foundation.Metadata.ApiInformation;
+
+            if (apiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 5)) {
+
+                var contacts = Windows.ApplicationModel.Contacts;
+
+                // Create a new contact annotation
+                var annotation = new contacts.ContactAnnotation();
+                annotation.contactId = contactId;
+                annotation.firstName = firstName;
+                annotation.lastName = lastName;
+                annotation.email = email;
+                annotation.phone = phone;
+
+                // Add appId and contact panel support to the annotation
+                var appId = "MyPeoplePWA_80c4904e66sn0";
+                annotation.ProviderProperties.add("ContactPanelAppID", appId);
+                annotation.SupportedOperations = contacts.ContactAnnotationOperations.contactProfile;
+
+                // Save annotation to contact annotation list
+                contacts.ContactAnnotationList.trySaveAnnotationAsync(annotation);
+            }
+        };
+
+        this.DeleteContact = function (contactId) {
+
+        };
+
+        this.GetContactList = function () {
+
+        };
+
+        this.GetContactAnnotationList = function () {
+
+        };
+    };
+
+    var myPeopleService = new MyPeopleService();
+    document.myPeople = myPeopleService;
+
+})();
