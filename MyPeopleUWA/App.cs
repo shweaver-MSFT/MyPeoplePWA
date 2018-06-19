115381207a8654f36298b9ea23e1ac493a8b9a09
REDMOND\shweaver <shweaver@microsoft.com>
2018-06-19 09:58:39 -0700

    Adding code to recursive copy Web content in to local storage (WIP)

--- MyPeopleUWA/App.cs
+++ MyPeopleUWA/App.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using WebView.Interop.UWP;
+using Windows.ApplicationModel;
+using Windows.Storage;
 
 namespace MyPeopleUWA
 {
@@ -7,10 +10,33 @@ namespace MyPeopleUWA
     {
         static void Main(string[] args)
         {
+            // Transfer all Web content to local cache folder.
+            // TODO: Consider clearing the cache first. May not be necessary
+            var webFolder = Package.Current.InstalledLocation.GetFolderAsync("Web").GetAwaiter().GetResult();
+            var destinationFolder = ApplicationData.Current.LocalCacheFolder;
+            RecursiveCopy(webFolder, destinationFolder).GetAwaiter().GetResult();
+
             Start(_ => new App());
         }
 
-        public App() : base(new Uri("ms-appx-web:///Web/index.html")) { }
+        public App() : base(new Uri("Web/index.html")) { }
+
+        private static async Task RecursiveCopy(IStorageItem storageItem, IStorageFolder destinationFolder)
+        {
+            if (storageItem is StorageFolder)
+            {
+                var temp = await destinationFolder.CreateFolderAsync(storageItem.Name, CreationCollisionOption.ReplaceExisting);
+                var items = await (storageItem as StorageFolder).GetItemsAsync();
+                foreach (var item in items)
+                {
+                    await RecursiveCopy(item, temp);
+                }
+            }
+            else if (storageItem is StorageFile)
+            {
+                await (storageItem as StorageFile).CopyAsync(destinationFolder, storageItem.Name, NameCollisionOption.ReplaceExisting);
+            }
+        }
     }
 }
 
