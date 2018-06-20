35dbc0ef089f0ce00f8ee1f51e53ccfe6f18da8b
Jim Gale <jimgaleforce@gmail.com>
2018-06-19 19:29:02 -0700

    still working w/out pics

--- MyPeopleUWA/App.cs
+++ MyPeopleUWA/App.cs
@@ -11,14 +11,14 @@ namespace MyPeopleUWA
         static void Main(string[] args)
         {
             // Transfer all Web content to local cache folder.
-            var webFolder = Package.Current.InstalledLocation.GetFolderAsync("Web").GetAwaiter().GetResult();
-            var destinationFolder = ApplicationData.Current.LocalCacheFolder;
-            RecursiveCopy(webFolder, destinationFolder).GetAwaiter().GetResult();
+            //var webFolder = Package.Current.InstalledLocation.GetFolderAsync("Web").GetAwaiter().GetResult();
+            //var destinationFolder = ApplicationData.Current.LocalCacheFolder;
+            //RecursiveCopy(webFolder, destinationFolder).GetAwaiter().GetResult();
 
             Start(_ => new App());
         }
 
-        public App() : base(new Uri("ms-appdata:///LocalCache/Web/index.html")) { } // ms-appx-web:///Web/index.html
+        public App() : base(new Uri("ms-appx:///Web/index.html")) { } // ms-appx-web:///Web/index.html
 
         private static async Task RecursiveCopy(IStorageItem storageItem, IStorageFolder destinationFolder)
         {
